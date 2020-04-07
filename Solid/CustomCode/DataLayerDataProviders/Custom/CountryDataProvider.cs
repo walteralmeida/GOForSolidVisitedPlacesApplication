8072670fa2cb568e19d9dc43c94756c58d743063
walteralmeida <walter.almeida@generativeobjects.com>
2020-04-08 00:37:39 +0200

    data provider Country

--- Solid/CustomCode/DataLayerDataProviders/Custom/CountryDataProvider.cs
+++ Solid/CustomCode/DataLayerDataProviders/Custom/CountryDataProvider.cs
@@ -9,15 +9,32 @@ using System.Linq.Expressions;
 using GenerativeObjects.Practices.LayerSupportClasses.DataLayer;
 using GenerativeObjects.Practices.ORMSupportClasses;
 using Solid.Data.DataObjects;
- 
+
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using VDS.RDF;
+using System.Linq;
+using GenerativeObjects.Practices.LayerSupportClasses;
+using Unity;
 
 namespace Solid.Data.DataProviders.Custom
 {
     public class CountryDataProvider : DataProvider<CountryDataObject>
     {
+
         protected override int DoCount(LambdaExpression securityFilterExpression, string filterPredicate, object[] filterArguments, IObjectsDataSet context, Dictionary<string, object> parameters)
         {
-            throw new NotImplementedException();
+            int count;
+
+            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
+            
+            //Make a SELECT query against the Endpoint
+            SparqlResultSet results = endpoint.QueryWithResultSet("SELECT count(?country) WHERE { ?country a <http://dbpedia.org/class/yago/WikicatMemberStatesOfTheUnitedNations> }");
+
+            var res = results.Single();
+            count = Convert.ToInt32((res.Single().Value as BaseLiteralNode).Value);
+
+            return count;
         }
 
         protected override void DoDelete(CountryDataObject entity, LambdaExpression securityFilterExpression, IObjectsDataSet context, Dictionary<string, object> parameters)
@@ -27,12 +44,93 @@ namespace Solid.Data.DataProviders.Custom
 
         protected override CountryDataObject DoGet(CountryDataObject entity, LambdaExpression securityFilterExpression, List<string> includes, IObjectsDataSet context, Dictionary<string, object> parameters)
         {
-            throw new NotImplementedException();
+            var uri = entity.URI.Replace("||", "://").Replace("|", "/");
+
+            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
+
+            string queryString = @"SELECT ?countryName, ?longName, ?flag, ?populationTotal, MAX(?populationDensity) as ?density ,  ?abstract
+                  WHERE {
+                                ?country rdfs:label ?countryName .
+
+                                FILTER langMatches(lang(?countryName), 'en')
+
+                                OPTIONAL {
+                                ?country dbo:longName ?longName.
+                                ?country dbo:abstract ?abstract.
+                                ?country dbo:flag ?flag.
+                                ?country dbo:populationTotal ?populationTotal .
+                                ?country dbo:populationDensity ?populationDensity
+
+                                FILTER langMatches(lang(?longName), 'en')
+                                FILTER langMatches(lang(?abstract), 'en')
+                                }
+                                }
+                                ";
+
+            queryString = queryString.Replace("?country ", $"<{uri}> ");
+
+            //Make a SELECT query against the Endpoint
+            SparqlResultSet results = endpoint.QueryWithResultSet(queryString);
+
+            var result = results.Single();
+
+            var country = new CountryDataObject();
+
+            country.URI = entity.URI;
+            country.Name = (result.Where(r => r.Key == "countryName").Single().Value as BaseLiteralNode).Value;
+            country.LongName = (result.Where(r => r.Key == "longName").Single().Value as BaseLiteralNode)?.Value;
+            country.Abstract = (result.Where(r => r.Key == "abstract").Single().Value as BaseLiteralNode)?.Value;
+            country.IsNew = false;
+            country.IsDirty = false;
+
+            return country;
         }
 
         protected override DataObjectCollection<CountryDataObject> DoGetCollection(LambdaExpression securityFilterExpression, string filterPredicate, object[] filterArguments, string orderByPredicate, int pageNumber, int pageSize, List<string> includes, IObjectsDataSet context, Dictionary<string, object> parameters)
         {
-            throw new NotImplementedException();
+            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
+
+            //Make a SELECT query against the Endpoint
+            SparqlResultSet results = endpoint.QueryWithResultSet(
+                @"SELECT ?country, ?countryName, ?longName, ?flag, ?populationTotal, MAX(?populationDensity) as ?density ,  ?abstract
+                  WHERE {
+                                ?country a <http://dbpedia.org/class/yago/WikicatMemberStatesOfTheUnitedNations> .
+                                ?country rdfs:label ?countryName .
+
+                                FILTER langMatches(lang(?countryName), 'en')
+
+                                OPTIONAL {
+                                ?country dbo:longName ?longName.
+                                ?country dbo:abstract ?abstract.
+                                ?country dbo:flag ?flag.
+                                ?country dbo:populationTotal ?populationTotal .
+                                ?country dbo:populationDensity ?populationDensity
+
+                                FILTER langMatches(lang(?longName), 'en')
+                                FILTER langMatches(lang(?abstract), 'en')
+                                }
+                                }
+                                ORDER BY ?countryName
+                                " + $"LIMIT {pageSize} OFFSET {(pageNumber-1)*pageSize}");
+
+            var toReturn = new DataObjectCollection<CountryDataObject>();
+            toReturn.ObjectsDataSet = ApplicationSettings.Container.Resolve<IObjectsDataSet>();
+
+            foreach (var result in results)
+            {
+                var country = new CountryDataObject();
+
+                country.URI = (result.Where(r => r.Key == "country").Single().Value as UriNode).Uri.ToString().Replace("://", "||").Replace("/", "|");
+                country.Name = (result.Where(r => r.Key == "countryName").Single().Value as BaseLiteralNode).Value;
+                country.LongName = (result.Where(r => r.Key == "longName").Single().Value as BaseLiteralNode)?.Value;
+                country.Abstract = (result.Where(r => r.Key == "abstract").Single().Value as BaseLiteralNode)?.Value;
+                country.IsNew = false;
+                country.IsDirty = false;
+
+                toReturn.Add(country);
+            }
+
+            return new DataObjectCollection<CountryDataObject>(toReturn);
         }
 
         protected override CountryDataObject DoSave(CountryDataObject entity, LambdaExpression securityFilterExpression, List<string> includes, IObjectsDataSet context, Dictionary<string, object> parameters)
