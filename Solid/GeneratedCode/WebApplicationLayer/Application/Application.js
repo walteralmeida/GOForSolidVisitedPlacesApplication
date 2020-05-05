c8cdc187b4d52457fc799650551a3b18dc1fa86b
GO Cloud <gocloud@generativeobjects.com>
2020-05-05 13:35:42 +0200

    CodeGenerator auto commit changes from Solid

--- Solid/GeneratedCode/WebApplicationLayer/Application/Application.js
+++ Solid/GeneratedCode/WebApplicationLayer/Application/Application.js
@@ -6,7 +6,7 @@
 (function (global) {
 	Solid.Web.Application = {
 		contextId : null,
-		buildNumber : 33 
+		buildNumber : 34 
 	};
 } (window));
 
--- Solid/GeneratedCode/WebApplicationLayer/Views/ViewLoader.js
+++ Solid/GeneratedCode/WebApplicationLayer/Views/ViewLoader.js
@@ -9,7 +9,7 @@
         var self = this;
 		// CORDOVA:
 		// var serviceUrl = "./ConstructedViews/";
-		var serviceUrl = Solid.Web.Application.BaseURL + "33/ConstructedViews/";
+		var serviceUrl = Solid.Web.Application.BaseURL + "34/ConstructedViews/";
 
         var loadViewRequest = null;
 
